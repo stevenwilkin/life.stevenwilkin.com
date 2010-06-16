22e590a1c560d29a23631ff23377488d03c9003a
Steven Wilkin <stevenwilkin@gmail.com>
2010-06-16 21:15:46 +0100

    final design with faked data

--- lifestream.rb
+++ lifestream.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'faker'
 
 class LifeStream < Sinatra::Base
 
@@ -12,6 +13,13 @@ class LifeStream < Sinatra::Base
 	set :logging, true
 
 	get '/' do
+		@items = []
+		1.upto(10).each do
+			date = Time.utc(2010, 1 + rand(12), 1+ rand(28), rand(24), rand(50))
+			url = 'http://' + Faker::Internet.domain_name() + '/' + Faker::Lorem.words(rand(6)).join('/')
+			type = %w(blog lastfm delicious)[rand(3)]
+			@items << {:type => type, :date => date, :url => url}
+		end
 		haml :index
 	end
 
